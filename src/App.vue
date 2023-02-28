4614886d7a1564cf26346b45dbb4fc4bd5b16a65
Ramy <ramy@mirzacles.tech>
2023-02-28 12:59:59 +0100

    Show config screen if not all localStorage keys are set

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,47 @@
 <template>
-  <Notes />
+  <Settings v-if="shouldShowConfig()" />
+  <Notes v-else />
 </template>
 
 <script>
+import Settings from './components/Settings.vue'
 import Notes from './components/Notes.vue'
 
 export default {
   name: 'App',
   components: {
+    Settings,
     Notes
-  }
+  },
+  data() {
+    return {
+      mode: 'config', // If mode == config, show config screen. If mode == notes, show notes.
+    };
+  },
+  methods: {
+    shouldShowConfig() {
+      return (
+        this.storageValues.op_connect_server_host === null ||
+        this.storageValues.op_api_token === null ||
+        this.storageValues.op_vault_uuid === null
+      );
+    },
+  },
+  mounted() {
+    
+  },
+  watch: {
+
+  },
+  computed: {
+    storageValues() {
+      return {
+        op_connect_server_host: localStorage.getItem('op_connect_server_host') || null,
+        op_api_token: localStorage.getItem('op_api_token') || null,
+        op_vault_uuid: localStorage.getItem('op_vault_uuid') || null,
+      };
+    },
+  },
 }
 </script>
 
---
+++ src/components/Settings.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="modal-backdrop fade show" v-if="showCreateNoteModal"></div>
+  <!-- Top row with app title -->
+  <div class="container-fluid bg-light py-3">
+      <div class="row">
+          <div class="col-12">
+            <div>
+              <h5><img src="/img/1password_icon.png" height="24" width="24"> {{title}}</h5>
+            </div>
+          </div>
+      </div>
+  </div>
+  
+  <!-- Settings screen -->
+  <div class="container-sm mt-3">
+      <div class="row">
+          <div class="col-12">
+            <form class="form-group">
+                <div class="mb-3">
+                    <label for="op_connect_server_host" class="form-label">1Password Connect Server Host</label>
+                    <input type="text" class="form-control" id="op_connect_server_host" name="op_connect_server_host" v-model="config.op_connect_server_host" required >
+                </div>
+                <div class="mb-3">
+                    <label for="op_api_token" class="form-label">1Password API Token</label>
+                    <input type="text" class="form-control" id="op_api_token" name="op_api_token" v-model="config.op_api_token" required >
+                </div>
+                <div class="mb-3">
+                    <label for="op_vault_uuid" class="form-label">1Password Vault UUID</label>
+                    <input type="text" class="form-control" id="op_vault_uuid" name="op_vault_uuid" v-model="config.op_vault_uuid" required>
+                </div>
+                <div class="mb-3">
+                    <button type="button" class="btn btn-success" id="btnStoreConfig" name="btnStoreConfig" @click="storeConfig()">Connect to 1Password</button>
+                </div>
+            </form>
+          </div>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MySettings',
+  data() {
+    return {
+      title: 'Notes',
+      config: {
+        op_connect_server_host: null,
+        op_api_token: null,
+        op_vault_uuid: null
+      }
+    };
+  },
+  methods: {
+    updateLocalStorage(key, value) {
+      localStorage.setItem(key, value);
+    },
+    storeConfig() {
+      this.updateLocalStorage('op_connect_server_host', this.config.op_connect_server_host);
+      this.updateLocalStorage('op_api_token', this.config.op_api_token);
+      this.updateLocalStorage('op_vault_uuid', this.config.op_vault_uuid);
+      window.location.reload();
+    },
+  },
+  mounted() {
+    
+  },
+  watch: {
+  },
+  computed: {
+    storageValues() {
+      return {
+        op_connect_server_host: localStorage.getItem('op_connect_server_host') || null,
+        op_api_token: localStorage.getItem('op_api_token') || null,
+        op_vault_uuid: localStorage.getItem('op_vault_uuid') || null,
+      };
+    },
+  },
+};
+</script>
